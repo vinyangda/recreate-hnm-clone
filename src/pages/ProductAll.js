34e87b735cc551e1105b16c4ee1bd2c368cbd10a
vin <didekqls93@gmail.com>
2023-02-23 14:15:41 +0900

    repactor : connect db.json

--- src/pages/ProductAll.js
+++ src/pages/ProductAll.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "../components/Header";
 import NavBar from "../components/NavBar";
 
 const ProductAll = () => {
+  const getProducts = async () => {
+    let url = `http://localhost:5003/products`;
+    let res = await fetch(url);
+    let data = await res.json();
+    console.log(data);
+  };
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
     <div>
-      <Header />
       <NavBar />
     </div>
   );
