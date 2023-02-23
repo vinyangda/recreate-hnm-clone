c7bedb55c9c917df385955e3b6b468897bfcf65f
vin <didekqls93@gmail.com>
2023-02-23 14:39:19 +0900

    feat : ceate product card

---
+++ src/components/ProductCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ProductCard = () => {
+  return (
+    <div>
+      <img src="https://lp2.hm.com/hmgoepprod?set=source[/31/c6/31c6b7f53c0a56ce15d0f337d67c4718133f5c33.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[y],hmver[2]&call=url[file:/product/main]" />
+      <div>Conscious Choice</div>
+      <div>Coat</div>
+      <div>999,990</div>
+      <div>New</div>
+    </div>
+  );
+};
+
+export default ProductCard;
--- src/pages/ProductAll.js
+++ src/pages/ProductAll.js
@@ -1,13 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import NavBar from "../components/NavBar";
+import ProductCard from "../components/ProductCard";
 
 const ProductAll = () => {
+  const [productList, setProductList] = useState();
+
   const getProducts = async () => {
     let url = `http://localhost:5003/products`;
     let res = await fetch(url);
     let data = await res.json();
-    console.log(data);
+    setProductList(data);
   };
   useEffect(() => {
     getProducts();
@@ -16,6 +19,7 @@ const ProductAll = () => {
   return (
     <div>
       <NavBar />
+      <ProductCard />
     </div>
   );
 };
